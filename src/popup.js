da23daabb5553e4f7219356aee5490b5197aa418
ChristianE00 <u1273549@utah.edu>
2024-01-05 01:35:05 -0700

    Updated home.html to use tailwind

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,25 @@
-// Listen for tab changes
-/*chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.storage.sync.set({ [activeInfo.tabId]: 100 });
+console.log("popup.js loaded--");
+
+/*
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello, world!'
+        };
+    },
+    render() {
+        return Vue.h('h1', this.message);
+    }
 });
+
+app.mount('#app');
+*/
 
 // Load the current volume for the tab
 document.addEventListener('DOMContentLoaded', function () {
     var rangeInput = document.getElementById('volumeSlider');
     var rangeValue = document.getElementById('rangeValue');
-
+    var messaging = chrome.runtime.sendMessage || browser.runtime.sendMessage;
     // Get the current tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         // Get the current volume for this tab
@@ -17,11 +29,8 @@ document.addEventListener('DOMContentLoaded', function () {
             if (volume == undefined) {
                 volume = 100;
             }
-            else {
-                updateVolume(volume);
-            }
             rangeValue.innerText = volume;
-            rangeInput.value     = volume;
+            rangeInput.value = volume;
         });
         // Listen for changes to the slider
         if (rangeInput && rangeValue) {
@@ -34,9 +43,20 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Elements not found');
         }
 
+
         document.getElementById('volumeSlider').addEventListener('change', function () {
+            var volume = this.value;
+            if (volume == undefined) {
+                console.log('volume undefined');
+                volume = 100;
+            }
+            var querying = chrome.tabs.query || browser.tabs.query;
             chrome.storage.sync.set({ [String(currentTab.id)]: this.value });
-            updateVolume(this.value);
+            querying({ active: true, currentWindow: true }, function (tabs) {
+                messaging({ command: "background", level: volume, tab: tabs[0].id }, function (response) {
+
+                });
+            });
         });
 
     });
@@ -44,15 +64,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
-function updateVolume(level) {
-    console.log("Volume: " + level);
-    var querying = chrome.tabs.query || browser.tabs.query;
-    var messaging = chrome.tabs.sendMessage || browser.tabs.sendMessage;
-    querying({ active: true, currentWindow: true }, function (tabs) {
-        messaging(tabs[0].id, { command: "volume", level: level }, function (response) {
-            console.log(response);
-        });
-    });
-}
-*/
