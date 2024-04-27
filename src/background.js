d6c955792a0007c3aca7d797b97ea7c36b006925
unknown <u1273549@utah.edu>
2024-04-27 01:08:33 -0600

    fixed switching tab bug which reset audio to 100%

--- src/background.js
+++ src/background.js
@@ -1,12 +1,19 @@
 // Listen for tab changes
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.storage.sync.set({ [activeInfo.tabId]: 100 });
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        chrome.storage.sync.get(tab.url, function(data) {
+            if (data[tab.url] === undefined) {
+                // No existing volume setting for this tab, set it to 100
+                chrome.storage.sync.set({ [tab.url]: 100 });
+            }
+        });
+    });
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' && tab.active) {
-        chrome.storage.sync.get(String(tabId), function(data) {
-            let volume = data[tabId] !== undefined ? data[tabId] : 100;
+        chrome.storage.sync.get(tab.url, function(data) {
+            let volume = data[tab.url] !== undefined ? data[tab.url] : 100;
             console.log("NEW PAGE LOADED OR REFRESHED DETECTED volume: ", volume);
             updateVolume(volume, tabId);
         });
@@ -19,9 +26,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         updateVolume(request.level, request.tab);
     }
     else if(request.command === "getTabInfo"){
-        let tabId = sender.tab.id;
-        chrome.storage.sync.get(String(tabId), function(data) {
-            let volume = data[tabId] !== undefined ? data[tabId] : 100;
+        chrome.storage.sync.get(sender.tab.url, function(data) {
+            let volume = data[sender.tab.url] !== undefined ? data[sender.tab.url] : 100;
             console.log("New audio source detected volume: ", volume);
             sendResponse({volume: volume});
         });
--- src/popup.js
+++ src/popup.js
@@ -24,10 +24,15 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         // Get the current volume for this tab
         var currentTab = tabs[0];
-        chrome.storage.sync.get(String(currentTab.id), function (data) {
-            var volume = data[currentTab.id];
+        var tabKey = String(currentTab.url); // Use the tab URL as the key
+        chrome.storage.sync.get(tabKey, function (data) {
+            var volume = data[tabKey];
             if (volume == undefined) {
+                console.log('Volume not found, NEW TAB')
                 volume = 100;
+            }
+            else{
+                console.log('volume NOT undefined: ' + volume)
             }
             rangeValue.innerText = volume;
             rangeInput.value = volume;
@@ -50,17 +55,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log('volume undefined');
                 volume = 100;
             }
+            console.log('saving slider: ' + volume)
             var querying = chrome.tabs.query || browser.tabs.query;
-            chrome.storage.sync.set({ [String(currentTab.id)]: this.value });
+            chrome.storage.sync.set({ [tabKey]: this.value }); // Save the value with the tab URL as the key
             querying({ active: true, currentWindow: true }, function (tabs) {
                 messaging({ command: "background", level: volume, tab: tabs[0].id }, function (response) {
-
+    
                 });
             });
         });
-
     });
-
-
 });
-
