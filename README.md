705266ce677aa07ef528d07d7803513c01ef4818
unknown <u1273549@utah.edu>
2024-04-27 11:10:05 -0600

    Local tailwindcss added and README

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+# Build steps:
+
+1. Build tailwindcss
+
+```bash
+npm run build
+```
+
+
+# Troubleshooting
+
+- All html and css files should be in the /src directory for tailwindcss to properly build the css file.
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /**
+   *  ** - A special part of the glob pattern that matches any number of directories. It 
+   *       will look in all subdirectories of src  
+   *   * - A wildcard that matches any file name 
+  * */
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
