80bc257c44323152e28e28a7d231583a2005f113
unknown <u1273549@utah.edu>
2024-05-08 14:53:59 -0600

    save/get function added

--- worker.js
+++ worker.js
@@ -7,6 +7,7 @@ chrome.runtime.onMessage.addListener(async (msg) => {
   console.log("Message received from popup");
   switch (msg.type) {
     case "popup-loaded":
+      //await chrome.storage.local.clear();
       let t = await getCurrentTab();
       var level = getTabLevel(t.id);
       console.log("[SERVICE-WORKER] Popup loaded message received sedning level: ", level);
@@ -32,21 +33,73 @@ chrome.runtime.onMessage.addListener(async (msg) => {
   }
 });
 
-async function testSave(tabId){
-  await chrome.storage.local.set({tabId: tabId}, function() {
-    console.log('[WORKER] Value is set to ' + tabId);
-  });
+async function saveTabLevel(tabId, level){
+  let items = await chrome.storage.local.get('levels');
+  let tabLevels = null;
+  // If levels is null, create a new object
+  if (items.levels == null){
+    tabLevels = {
+      [tabId]: level
+    };
+  }
+  // Otherwise, update the existing object
+  else{
+    tabLevels = items.levels;
+    tabLevels[tabId] = level;
+  }
+
+  await chrome.storage.local.set({levels: tabLevels});
 }
 
-async function testGet(tabId){
-  const items = await chrome.storage.local.get('tabId');
-  if (items.tabId) {
-    console.log('[WORKER] Value found currently is ' + items.tabId);
+async function getTabLevel(tabId){
+  let items = await chrome.storage.local.get('levels');
+  let tabLevels = items.levels;
+  // If level for current tab is not found, return -1
+  if (tabLevels == null){
+    return -1;
   }
   else{
-    console.log('[WORKER] Value not found');
+    return tabLevels[tabId] * 100;
   }
 }
+
+/*
+async function testSave(tabId){
+  //clear storage
+  let items = await chrome.storage.local.get('levels');
+  let tabLevels = null;
+
+  if(items.levels == null){
+    console.log('[WORKER] levels is null');
+    tabLevels = {
+      ["1"]: "100",
+      ["2"]: "50"
+    };
+  }
+  else{
+    tabLevels = items.levels;
+    tabLevels[tabId] = "100";
+  }
+  await chrome.storage.local.set({levels: tabLevels} );
+}
+*/
+
+/*
+async function testGet(tabId){
+  // const items = await chrome.storage.local.get('tabId');
+   const items = await chrome.storage.local.get('levels');
+  // const item = await chrome.storage.local.get("2");
+  // console.log('[WORKER] value is ', item);
+   console.log('[WORKER] values are ', JSON.stringify(items.levels));
+  
+  //if (items.tabId) {
+    //console.log('[WORKER] Value found currently is ' + items.tabId);
+  //}
+  //else{
+    //console.log('[WORKER] Value not found');
+  //}
+}
+*/
 
 
 async function updateTabVolume(tabId, volume){
